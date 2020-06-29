a144f231f02efdcff528618000d5cf9bbfc3701e
Volodymyr Kryvonos <volodymyr.kryvonos@gmail.com>
2020-06-29 15:29:46 +0300

    Created custom transform stream which limit size of transfered data: 2-module 1-task

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -2,12 +2,30 @@ const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
+  _limit;
+  _transferredDataSize = 0;
+
   constructor(options) {
+    if (options === undefined) {
+      throw new TypeError('object with options should be specified');
+    }
+    if (options.limit === undefined) {
+      throw new TypeError('limit size should be specified');
+    }
     super(options);
+    this._limit = options.limit;
   }
 
   _transform(chunk, encoding, callback) {
+    const potentialSize = this._transferredDataSize + chunk.length;
+    if (potentialSize > this._limit) {
+      callback(new LimitExceededError());
+      return;
+    }
 
+    this.push(chunk);
+    this._transferredDataSize = potentialSize;
+    callback();
   }
 }
 
