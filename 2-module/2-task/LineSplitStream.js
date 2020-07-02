4d8e8e5fb134254d914dbe8f880035e1772aca0b
Volodymyr Kryvonos <volodymyr.kryvonos@gmail.com>
2020-07-02 14:55:51 +0300

    Created custom stream which reads string's streams by lines: 2-module 2-task

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -2,14 +2,48 @@ const stream = require('stream');
 const os = require('os');
 
 class LineSplitStream extends stream.Transform {
+  _strWithoutEOL = '';
+
   constructor(options) {
     super(options);
   }
 
   _transform(chunk, encoding, callback) {
+    let lastIndexOf = 0;
+    let currIndexOf = -1;
+
+    /*
+     * if there is match of EOL in chunk
+     *   then
+     *    slice from lastMath to indexOf + 1
+     *   else
+     *    slice part of chunk from lastMatch to the end of line
+     *    and save it into variable
+     */
+    while(true) {
+      currIndexOf = chunk.indexOf(os.EOL, lastIndexOf);
+
+      if (currIndexOf === -1) {
+        this._strWithoutEOL += chunk.slice(lastIndexOf);
+
+        break;
+      }
+
+      this.push(this._strWithoutEOL + chunk.slice(lastIndexOf, currIndexOf));
+
+      lastIndexOf = currIndexOf + 1;
+      this._strWithoutEOL = '';
+    }
+
+    callback();
   }
 
   _flush(callback) {
+    if (this._strWithoutEOL) {
+      this.push(this._strWithoutEOL);
+      this._strWithoutEOL = '';
+    }
+    callback();
   }
 }
 
