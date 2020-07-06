2b7ac6270462fa88ad42a8165613d13cdd7f267b
Volodymyr Kryvonos <volodymyr.kryvonos@gmail.com>
2020-07-06 18:29:56 +0300

    Implemented removing of files: 4-module 3-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,17 +1,32 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'DELETE':
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end('Nested paths are not allowed');
+      }
 
+      fs.unlink(filepath, (err) => {
+        if (err && err.code === 'ENOENT') {
+          res.statusCode = 404;
+          return res.end('File not found');
+        } else if (err) {
+          res.statusCode = 500;
+          res.end('Internal Server Error');
+        }
+
+        res.end();
+      });
       break;
 
     default:
